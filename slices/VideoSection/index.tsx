1cf77ee18f4f59930379b1059ac9dfa02665458d
SNOWHITE\pacu <sibeeriano@gmail.com>
2026-01-14 02:19:44 -0300

    Fix: Unificar estructura de VideoSection slice para consistencia con otros slices

--- slices/VideoSection/index.tsx
+++ slices/VideoSection/index.tsx
@@ -1,19 +1,2 @@
-import * as React from "react"
-import { SliceComponentProps } from "@prismicio/react"
-import VideoSection from "@/app/components/VideoSection"
-import * as prismic from "@prismicio/client"
-
-export type VideoSectionSlice = prismic.Content.VideoSectionSlice
-
-export default function VideoSectionSliceComponent({
-  slice,
-}: SliceComponentProps<VideoSectionSlice>) {
-  const videoUrl = prismic.asLink(slice.primary.url_video) || undefined
-
-  return (
-    <VideoSection
-      videoUrl={videoUrl}
-      title="Video"
-    />
-  )
-}
+export { default } from "@/app/slices/VideoSection";
+export type { VideoSectionSlice } from "@/app/slices/VideoSection";
